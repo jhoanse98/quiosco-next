fb345847e8c6eacc4db39d560b74abe321e7f40d
Sebas24 <sebastian.marin@lernit.com.mx>
2025-01-31 21:38:20 -0500

    feat: card de producto

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,11 +1,39 @@
+import ProductCard from "@/components/products/ProductCard";
+import { prisma } from "@/src/lib/prisma";
+
 type CategoryProps = {
   category: string;
 };
 
+const getProducts = async (category: string) => {
+  const products = await prisma.product.findMany({
+    where: {
+      category: {
+        slug: category,
+      },
+    },
+  });
+
+  return products;
+};
+
 const OrderPage = async ({ params }: { params: Promise<CategoryProps> }) => {
   const { category } = await params;
+  const products = await getProducts(category);
+  console.log("los products", products);
   console.log("el params", category);
-  return <div>OrderPage</div>;
+  return (
+    <>
+      <h1 className="text-2xl my-10">
+        Elige y personaliza tu pedido a continuación
+      </h1>
+      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default OrderPage;
