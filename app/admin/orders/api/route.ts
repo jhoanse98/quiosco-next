1052a9d1732c68e04cea8b96321035cc1cb04cb9
Sebas24 <sebastian.marin@lernit.com.mx>
2025-02-23 18:04:43 -0500

    fix: variable no usada ts

--- app/admin/orders/api/route.ts
+++ app/admin/orders/api/route.ts
@@ -1,8 +1,11 @@
 import { prisma } from "@/src/lib/prisma";
 
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const dynamic = 'force-dynamic';
 
 export async function GET() {
+  
       const orders = await prisma.order.findMany({
         where: {
           status: false,
--- app/orders/api/route.tsx
+++ app/orders/api/route.tsx
@@ -1,5 +1,6 @@
 import { prisma } from "@/src/lib/prisma";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const dynamic = "force-dynamic";
 
 export async function GET() {
